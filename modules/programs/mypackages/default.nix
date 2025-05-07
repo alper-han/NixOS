355e0eecdd49b7e9182edbca7843b88c13ebcb51
alper-han <89567766+alper-han@users.noreply.github.com>
2025-05-07 04:51:58 +0300

    dotnet10 removed

--- modules/programs/mypackages/default.nix
+++ modules/programs/mypackages/default.nix
@@ -4,9 +4,7 @@
       home.packages = with pkgs; [
         jetbrains.rider # dotnet
         dotnet-sdk_9
-        dotnet-sdk_10
         dotnet-runtime_9        
-        dotnet-runtime_10
         
         remmina # rdp&vnc
         qbittorrent # torrent
