c699755db903e5a59f0139a4e8545e38637d1ac9
alper-han <89567766+alper-han@users.noreply.github.com>
2025-06-14 15:47:23 +0300

    gtk fix

--- modules/themes/rose-pine/default.nix
+++ modules/themes/rose-pine/default.nix
@@ -41,14 +41,10 @@ in
             name = "Papirus-Dark";
           };
           gtk3.extraConfig = {
-            Settings = ''
-              gtk-application-prefer-dark-theme=1
-            '';
+            "gtk-application-prefer-dark-theme" = "1";
           };
           gtk4.extraConfig = {
-            Settings = ''
-              gtk-application-prefer-dark-theme=1
-            '';
+            "gtk-application-prefer-dark-theme" = "1";
           };
         };
 
